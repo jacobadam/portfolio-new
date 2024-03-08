f2a714de6df2bfc5c93ef0ed124ae848c626910c
jacobadam <jacobanevitt@gmail.com>
2024-03-07 21:42:04 -0800

    update deploy script again

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,19 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# Initialize a new Git repository inside the "dist" directory
 git init
+
+echo "jacobnevitt.com" > CNAME
+
+# Add all files to the repository
 git add -A
+
+# Commit the changes
 git commit -m 'deploy'
 
-# push to the custom domain
-git push -f git@github.com:jacobnevitt.com $(git symbolic-ref --short HEAD):gh-pages
+# Push to the "gh-pages" branch of the repository
+git push -f git@github.com:jacobadam/portfolio-new.git master:gh-pages
 
+# Navigate back to the original directory
 cd -

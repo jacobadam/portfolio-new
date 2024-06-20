39400f1d1670b5a4df03ee2d125f4e94d3d15de2
jacobadam <jacobanevitt@gmail.com>
2024-06-20 14:13:53 -0700

    add images, add projects and css styling

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -3,35 +3,65 @@
     <h2>Projects</h2>
     <div class="projects">
       <div v-for="project in projects" :key="project.name" class="project">
-        <a :href="project.link" class="project-link">
+        <a
+          :href="project.link"
+          class="project-link"
+          target="_blank"
+          rel="noreferrer"
+        >
           <div class="project-content">
-            <img
-              :src="project.image"
-              :alt="project.name"
-              class="project-image"
-            />
+            <img :src="project.image" :alt="project.name" />
             <div class="project-details">
               <h3>{{ project.name }}</h3>
               <p class="project-description">{{ project.description }}</p>
             </div>
           </div>
         </a>
+        <div class="technology-list">
+          <div
+            class="technology-bubble"
+            v-for="tech in project.technologies"
+            :key="tech"
+          >
+            {{ tech }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import tetris from "@/assets/tetris.jpg";
+import podiatrist from "@/assets/nnwebsite.webp";
+import spotify from "@/assets/spotify-stats.webp";
+import tetris from "@/assets/tetris-site.webp";
+
 import { ref } from "vue";
 
 const projects = ref([
+  {
+    name: "Health Care Website",
+    description:
+      "A React app that integrates the Google Maps API and utilizes EmailJS for the contact form, with styling implemented using Tailwind CSS.",
+    image: podiatrist,
+    link: "https://jacobadam.github.io/nn-podiatry/",
+    technologies: ["ReactJS", "Tailwind", " GoogleMaps API", "EmailJS"],
+  },
+  {
+    name: "Spotify Stats",
+    description:
+      "A React application integrated with the Spotify API, utilizing an Express server hosted on Heroku to handle the Authorization Code Flow. The front end, hosted on Netlify, allows users to view their top artists, top tracks, and recently played tracks.",
+    image: spotify,
+    link: "https://listening-stats-jn.netlify.app/",
+    technologies: ["React", "Express", "Spotify API", "Heroku", "Netlify"],
+  },
   {
     name: "Tetris",
     description:
       "A tetris-style game built with a combination of JavaScript, HTML, and CSS.",
     image: tetris,
     link: "https://jacobadam.github.io/tetris/",
+    technologies: ["JavaScript", "HTML", "CSS"],
   },
 ]);
 </script>
@@ -42,8 +72,9 @@ h2 {
   padding-top: 30px;
 }
 
-.projects {
+.project {
   display: flex;
+  flex-direction: column;
   max-width: 450px;
   margin: 0 auto;
   border-radius: 8px;
@@ -78,10 +109,6 @@ h2 {
   }
 }
 
-.project {
-  margin-bottom: 20px;
-}
-
 .project-content {
   display: flex;
   align-items: center;
@@ -99,5 +126,22 @@ h2 {
 
 .project-details h3 {
   margin-bottom: 8px;
+}
+
+.technology-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 12px;
+  font-size: 10px;
+}
+
+.technology-bubble {
+  background-color: #4a5568;
+  color: coral;
+  border-radius: 18px;
+  padding: 8px 10px;
+  margin-right: 6px;
+  margin-bottom: 8px;
+  font-weight: bold;
 }
 </style>

80032e09021ac7e5e83297af661bb3ebf67dc240
jacobadam <jacobanevitt@gmail.com>
2024-09-18 14:28:20 -0700

    add service-worker for caching

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@ import App from "./App.vue";
 import "./global.css";
 
 createApp(App).mount("#app");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}

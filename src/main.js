7adf34d32aa8ce6c43365a0fa9b666512c1193f1
jacobadam <jacobanevitt@gmail.com>
2024-09-19 13:46:41 -0700

    comment out service-worker

--- src/main.js
+++ src/main.js
@@ -4,22 +4,22 @@ import "./global.css";
 
 createApp(App).mount("#app");
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        if (process.env.NODE_ENV === "development") {
-          console.log(
-            "Service Worker registered with scope:",
-            registration.scope
-          );
-        }
-      })
-      .catch((error) => {
-        if (process.env.NODE_ENV === "development") {
-          console.error("Service Worker registration failed:", error);
-        }
-      });
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/service-worker.js")
+//       .then((registration) => {
+//         if (process.env.NODE_ENV === "development") {
+//           console.log(
+//             "Service Worker registered with scope:",
+//             registration.scope
+//           );
+//         }
+//       })
+//       .catch((error) => {
+//         if (process.env.NODE_ENV === "development") {
+//           console.error("Service Worker registration failed:", error);
+//         }
+//       });
+//   });
+// }

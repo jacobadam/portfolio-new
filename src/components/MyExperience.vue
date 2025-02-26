856dfb277ce91fd7c2a13e2bdfd1fcd8c3b339ec
jacobadam <jacobanevitt@gmail.com>
2025-02-26 10:49:45 -0800

    update job title

--- src/components/MyExperience.vue
+++ src/components/MyExperience.vue
@@ -20,7 +20,7 @@ import ExperienceComponent from "./ExperienceComponent.vue";
 const jobs = ref([
   {
     company: "Article",
-    position: "Front-end Software Engineer",
+    position: "Front-end Software Developer",
     description:
       "I served as a front-end developer, actively participating in the maintenance and enhancement of a Vue powered eCommerce website. I specialized in integrating innovative technologies such as Apollo GraphQL for optimized data fetching, Algolia for seamless search functionality, and Contentful for effective content management.",
     dates: "2021-2024",
@@ -36,7 +36,7 @@ const jobs = ref([
   },
   {
     company: "SpareRoom",
-    position: "Back-end Software Engineer",
+    position: "Back-end Software Developer",
     description:
       "I actively contributed to the development of RESTful APIs using Perl, engaging in both front-end and back-end work for an in-house admin tool. With MySQL, I gathered data and created custom reports, showcasing my hands-on experience. I honed my skills with Git and Linux shell for version control, ensuring smooth collaboration. These experiences reflect my growth as a junior developer, demonstrating versatility and a practical understanding of key tools in software development.",
     dates: "2019-2020",
@@ -44,7 +44,7 @@ const jobs = ref([
   },
   {
     company: "Northcoders",
-    position: "Graduate Software Engineer",
+    position: "Graduate Software Developer",
     description:
       "I completed a comprehensive full-stack coding course at an award-winning bootcamp in Manchester. Acquired practical skills through an industry-led curriculum covering TDD, paired programming, and object-oriented programming. Collaborated within a group of 5, successfully delivering an end-to-end application in an agile environment. Engaged in daily stand-ups, utilized a kanban board, and presented the final project, showcasing my proficiency and adaptability as a junior developer.",
     dates: "2019",

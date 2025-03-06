a23422c2309295329d5e4b603ebdc46260f6b592
jacobadam <jacobanevitt@gmail.com>
2025-03-06 14:44:43 -0800

    remove service worker related code

--- src/main.js
+++ src/main.js
@@ -3,23 +3,3 @@ import App from "./components/App.vue";
 import "./global.css";
 
 createApp(App).mount("#app");
-
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker
-//       .register("/service-worker.js")
-//       .then((registration) => {
-//         if (process.env.NODE_ENV === "development") {
-//           console.log(
-//             "Service Worker registered with scope:",
-//             registration.scope
-//           );
-//         }
-//       })
-//       .catch((error) => {
-//         if (process.env.NODE_ENV === "development") {
-//           console.error("Service Worker registration failed:", error);
-//         }
-//       });
-//   });
-// }

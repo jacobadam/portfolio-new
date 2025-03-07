301969144d52e95888e76ea93352a55500a86a22
jacobadam <jacobanevitt@gmail.com>
2025-03-07 08:25:07 -0800

    install full ts particles package and initialise it

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from "vue";
 import App from "./components/App.vue";
 import "./global.css";
+import Particles from "@tsparticles/vue3";
+import { loadFull } from "tsparticles";
+
+createApp(App).use(Particles, {
+  init: async (engine) => {
+    await loadFull(engine);
+  },
+});
 
 createApp(App).mount("#app");
